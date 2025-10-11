b2dd8a5d6e46d789066059e4b6c5ecd8990e2b31
KhanhDepTrai <Khanhtqph42311@gmail.com>
2025-10-11 22:05:37 +0700

    khanh ok kkk
    Signed-off-by: KhanhDepTrai <Khanhtqph42311@gmail.com>

--- src/app/services/hoa-don.service.ts
+++ src/app/services/hoa-don.service.ts
@@ -13,7 +13,7 @@ export class HoaDonService {
   constructor(private http: HttpClient) { }
 
 
-//kkh
+//kkhj
   getAllHoaDon(): Observable<HoaDonDTO[]> {
     return this.http.get<HoaDonDTO[]>(this.apiUrl);
   }
